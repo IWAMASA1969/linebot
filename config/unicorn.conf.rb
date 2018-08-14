28ffecf750654103e9a5a7c4ec3268c79635a7ef
Hitoshi Iwamasa <masaxyz_labo@icloud.com>
2018-08-14 19:17:29 +0900

    modify unicorn.conf.rb. change socket directory and using ENV.

--- config/unicorn.conf.rb
+++ config/unicorn.conf.rb
@@ -10,7 +10,7 @@
 
 # Use at least one worker per core if you're on a dedicated server,
 # more will usually help for _short_ waits on databases/caches.
-APP_PATH = '/home/linebot/app_root'.freeze
+APP_PATH = ENV['LINEBOT_APP_PATH']
 
 worker_processes 4
 
@@ -27,20 +27,20 @@ working_directory APP_PATH # available in 0.94.0+
 
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
-listen APP_PATH + '/tmp/.unicorn.sock', backlog: 64
+listen '/tmp/linebot.sock', backlog: 64
 listen 8080, tcp_nopush: true
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
 timeout 30
 
 # feel free to point this anywhere accessible on the filesystem
-pid APP_PATH + '/tmp/pids/unicorn.pid'
+pid "#{APP_PATH}/tmp/pids/unicorn.pid"
 
 # By default, the Unicorn logger will write to stderr.
 # Additionally, ome applications/frameworks log to stderr or stdout,
 # so prevent them from going to /dev/null when daemonized here:
-stderr_path APP_PATH + '/log/unicorn.stderr.log'
-stdout_path APP_PATH + '/log/unicorn.stdout.log'
+stderr_path "#{APP_PATH}/log/unicorn.stderr.log"
+stdout_path "#{APP_PATH}/log/unicorn.stdout.log"
 
 # combine Ruby 2.0.0+ with "preload_app true" for memory savings
 preload_app true
@@ -59,7 +59,7 @@ run_once = true
 before_fork do |server, worker|
   # the following is highly recomended for Rails + "preload_app true"
   # as there's no need for the master process to hold a connection
-  defined?(ActiveRecord::Base) and
+  defined?(ActiveRecord::Base) &&
     ActiveRecord::Base.connection.disconnect!
 
   # Occasionally, it may be necessary to run non-idempotent code in the
@@ -101,7 +101,7 @@ after_fork do |server, worker|
   # server.listen(addr, :tries => -1, :delay => 5, :tcp_nopush => true)
 
   # the following is *required* for Rails + "preload_app true",
-  defined?(ActiveRecord::Base) and
+  defined?(ActiveRecord::Base) &&
     ActiveRecord::Base.establish_connection
 
   # if preload_app is true, then you may also want to check and
