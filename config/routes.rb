670e018377f6de81f3def384cef2cd89ab48c3b4
Hitoshi Iwamasa <masaxyz_labo@icloud.com>
2018-08-19 21:00:12 +0900

    add echo sample

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/hear', to: 'hear#create'
+  post '/callback', to: 'echo#callback'
 end
