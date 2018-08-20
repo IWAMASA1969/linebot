0e0eb74fa5fed409959317e61e42d34d2d9f87a5
Hitoshi Iwamasa <masaxyz_labo@icloud.com>
2018-08-20 22:55:22 +0900

    refactoring echo bot

---
+++ app/services/linebot_wrapper.rb
@@ -0,0 +1,32 @@
+class LinebotWrapper
+  require 'line/bot'
+
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
+  end
+
+  def validate_signature(body, signature)
+    false unless client.validate_signature(body, signature)
+  end
+
+  def echo(body)
+    events = client.parse_events_from(body)
+
+    events.each { |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+          client.reply_message(event['replyToken'], message)
+        end
+      end
+    }
+  end
+end
